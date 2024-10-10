51463b1aef2412173bae3c1fcf426eeb0720bc84
BEvgeniyB <masterbe2012@yandex.ru>
2024-10-10 22:08:46 +0300

    Update task.py

--- app/models/task.py
+++ app/models/task.py
@@ -12,6 +12,7 @@ class Task(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     content = Column(String)
+    priority = Column(Integer,default=0)
     completed = Column(BOOLEAN, default=False)
     user_id = Column(Integer, ForeignKey("users.id"))
     slug = Column(String, unique=True, index=True)
